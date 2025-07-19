c5979828e9e69e6364fd7907429146d23a9ebf24
naveenkumar061 <devarashettynaveen224@gmail.com>
2025-07-19 18:16:08 +0530

    CHALLENGE #3 done

--- script.js
+++ script.js
@@ -316,13 +316,13 @@ function calcAverageHumanAge(dogAge) {
 }
 console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
 console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
-*/
 
 const inrToUsd = 0.012;
 
 // PIPELINE
 const totalDepositsUSD = movements
-  .filter(mov => mov > 0)
-  .map(mov => mov * inrToUsd)
-  .reduce((acc, mov) => acc + mov, 0);
+.filter(mov => mov > 0)
+.map(mov => mov * inrToUsd)
+.reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositsUSD);
+*/
