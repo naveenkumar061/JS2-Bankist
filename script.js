b3b740a18bdc451a5ebabcd31e5e98cf43ae6cfc
naveenkumar061 <devarashettynaveen224@gmail.com>
2025-07-19 18:12:51 +0530

    The Magic of Chaining Methods done

--- script.js
+++ script.js
@@ -88,6 +88,25 @@ const displayMovements = function (movements) {
 };
 displayMovements(account1.movements);
 
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `₹ ${incomes}`;
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `₹ ${Math.abs(out)}`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter(int => int >= 100)
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `₹ ${interest}`;
+};
+calcDisplaySummary(account1.movements);
+
 const createUsernames = function (accs) {
   accs.forEach(acc => {
     acc.username = acc.owner
@@ -288,13 +307,22 @@ TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
 TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 
 GOOD LUCK 😀
-*/
 
 function calcAverageHumanAge(dogAge) {
   return dogAge
-    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
-    .filter(humanAge => humanAge >= 18)
-    .reduce((acc, age, index, arr) => acc + age / arr.length, 0);
+  .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+  .filter(humanAge => humanAge >= 18)
+  .reduce((acc, age, index, arr) => acc + age / arr.length, 0);
 }
 console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
 console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+*/
+
+const inrToUsd = 0.012;
+
+// PIPELINE
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map(mov => mov * inrToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
