ffce58529fca02608d8bfb38866ba82279673a69
naveenkumar061 <devarashettynaveen224@gmail.com>
2025-07-24 19:45:30 +0530

    More Ways of Creating and Filling Arrays done

--- script.js
+++ script.js
@@ -473,7 +473,7 @@ console.log(movements);
 // });
 movements.sort((a, b) => b - a);
 console.log(movements);
-*/
+
 
 console.log(movements);
 
@@ -494,3 +494,34 @@ console.log(groupedByActivity);
 
 const groupedAccountsByType = Object.groupBy(accounts, ({ type }) => type);
 console.log(groupedAccountsByType);
+*/
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// Empty + fill
+const x = new Array(7);
+console.log(x);
+// console.log(x.map(() => 5));
+// x.fill(9);
+x.fill(1, 3, 5);
+console.log(x);
+
+arr.fill(23, 4, 5);
+console.log(arr);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('₹', ''))
+  );
+
+  console.log(movementsUI);
+});
