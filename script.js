deb1106bc16425cdb968b37eb298c40b02441874
naveenkumar061 <devarashettynaveen224@gmail.com>
2025-07-24 11:55:33 +0530

    flat and flatMap done

--- script.js
+++ script.js
@@ -415,10 +415,27 @@ console.log(account);
 console.log(movements);
 console.log(movements.findLast(mov => mov < 0));
 console.log(movements.findLastIndex(mov => mov < 0));
-*/
 
 console.log(movements);
 console.log(movements.includes(-34476));
 
 console.log(movements.some(mov => mov > 0));
 console.log(movements.every(mov => mov > 0));
+*/
+
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+const overalBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance);
+
+const overalBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance2);
